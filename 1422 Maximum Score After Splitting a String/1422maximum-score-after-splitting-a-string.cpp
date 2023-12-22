bfa504467363b71e46277f39d931cfb514664ed7
Fady <fadyg6848@gmail.com>
2023-12-22 13:20:24 +0000

    Code challenge submission for question: 1422 Maximum Score After Splitting a String

---
+++ 1422 Maximum Score After Splitting a String/1422maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxScore(string s) {
+            int ones = 0;
+    for (int i = 0; i < s.size(); i++)
+        if (s[i] == '1')
+            ones++;
+    
+    int zeros = 0;
+    int ans = 0;
+    for (int i = 0; i < s.size() - 1; i++)
+    {
+        if (s[i] == '0')
+            zeros++;
+        else
+            ones--;
+
+        ans = max(ans, zeros + ones);
+    }
+    return ans;
+    }
+};
