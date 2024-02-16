1014dc17db0007d1d166f4d806009674415a9183
Fady <fadyg6848@gmail.com>
2024-02-16 12:04:41 +0000

    Code challenge submission for question: 75 Sort Colors

---
+++ 75 Sort Colors/75sort-colors.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> sortColors(vector<int>& nums) {
+        int zeros = 0, ones = 0, twos = 0;
+        for (auto i : nums)
+        {
+            if (i == 0)
+                zeros++;
+            else if (i == 1)
+                ones++;
+            else
+                twos++;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (zeros)
+            {
+                nums[i] = 0;
+                zeros--;
+            }
+            else if (ones)
+            {
+                nums[i] = 1;
+                ones--;
+            }
+            else
+            {
+                nums[i] = 2;
+            }
+        }
+        return nums;
+    }
+};
