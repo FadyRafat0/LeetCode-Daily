ad4c3cbe7e0011dbd2bd1fea0b6524c2bdce0f06
Fady Rafat <121770425+FadyRafat0@users.noreply.github.com>
2026-03-22 06:59:05 +0200

    Time: 0 ms (100.00%) | Memory: 15.1 MB (9.39%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -2,13 +2,6 @@ class Solution {
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
         for (int i = 0; i < 4; ++i) {
-            for (int i = 0; i < mat.size(); ++i) {
-                for (int j = 0; j < mat[0].size(); ++j) {
-                    cout << mat[i][j] << ' ';
-                }
-                cout << '\n';
-            }
-            cout << '\n';
             if (mat == target) 
                 return true;
 
@@ -16,9 +9,8 @@ public:
             vector<vector<int>> tmp;
             for (int j = 0; j < m; ++j) {
                 tmp.emplace_back();
-                for (int i = n - 1; i >= 0; --i) {
+                for (int i = n - 1; i >= 0; --i)
                     tmp.back().emplace_back(mat[i][j]);
-                }
             }
             mat = tmp;
         }
