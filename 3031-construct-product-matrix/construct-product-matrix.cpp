d8561376425fb5f9a432fa20fe3d944e19b0e5c3
Fady Rafat <121770425+FadyRafat0@users.noreply.github.com>
2026-03-24 07:27:59 +0200

    Time: 120 ms (9.96%) | Memory: 154.6 MB (24.35%) - LeetSync

---
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int const mod = 12345;
+    int mul(int a, int b) {
+        return (1ll * a * b) % mod;
+    }
+    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<int> prf(n + 3, 1), suf(n + 3, 1);
+        for (int i = 1; i <= n; ++i) {
+            int x = 1;
+            for (int j = 1; j <= m; ++j) {
+                x = mul(x, grid[i - 1][j - 1]);
+            }
+            prf[i] = mul(prf[i - 1], x);
+        }
+        for (int i = n; i >= 1; --i) {
+            int x = 1;
+            for (int j = 1; j <= m; ++j) {
+                x = mul(x, grid[i - 1][j - 1]);
+            }
+            suf[i] = mul(suf[i + 1], x);
+        }
+
+        vector<vector<int>> ans(n, vector<int>(m));
+        for (int i = 1; i <= n; ++i) {
+            int cur = mul(prf[i - 1], suf[i + 1]);
+            vector<int> cur_prf(m + 2, 1), cur_suf(m + 2, 1);
+            for (int j = 1; j <= m; ++j) {
+                cur_prf[j] = mul(cur_prf[j - 1], grid[i - 1][j - 1]);
+            }
+            for (int j = m; j >= 1; --j) {
+                cur_suf[j] = mul(cur_suf[j + 1], grid[i - 1][j - 1]);
+            }
+
+            for (int j = 1; j <= m; ++j) {
+                ans[i - 1][j - 1] = mul(cur, mul(cur_prf[j - 1], cur_suf[j + 1]));
+            }
+        }
+
+        return ans;
+    }
+};
