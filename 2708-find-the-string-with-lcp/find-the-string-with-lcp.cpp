afbc70748cdbd6bde7b3628a77648c1b5dc39fad
Fady Rafat <121770425+FadyRafat0@users.noreply.github.com>
2026-03-28 08:20:56 +0200

    Time: 27 ms (26.51%) | Memory: 94.6 MB (6.02%) - LeetSync

---
+++ 2708-find-the-string-with-lcp/find-the-string-with-lcp.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int rec(int i, int j, string &s) {
+        if (max(i, j) >= s.size()) return 0;
+        int &ret = dp[i][j];
+        if (~ret) return ret;
+        ret = (s[i] == s[j] ? rec(i + 1, j + 1, s) + 1 : 0);
+        return ret;
+    }
+    string findTheString(vector<vector<int>>& lcp) {        
+        int n = lcp.size();
+        string s(n, '0');
+        for (int i = 0; i < n; ++i) {
+            vector<bool> vis(26);
+            for (int j = 0; j < i; ++j) {
+                if (lcp[i][j]) {
+                    s[i] = s[j];
+                    break;
+                }
+                else {
+                    vis[s[j] - 'a'] = 1;
+                }
+            }
+
+            if (s[i] != '0') continue;
+            int ch = -1;
+            for (int j = 0; j < 26; ++j) {
+                if (!vis[j]) {
+                    ch = j; break;
+                }
+            }
+
+            if (ch == -1) return "";
+
+            s[i] = ch + 'a';
+        }
+
+        dp = vector<vector<int>>(n, vector<int>(n, -1));
+        vector<vector<int>> my_lcp(n, vector<int>(n));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                my_lcp[i][j] = rec(i, j, s);
+            }
+        }
+
+        return (lcp == my_lcp ? s : "");
+    }
+};
