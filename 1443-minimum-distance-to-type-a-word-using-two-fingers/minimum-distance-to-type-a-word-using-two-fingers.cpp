04e37e400fd82b2d1811d662c3b83f5264b58d0c
Fady Rafat <121770425+FadyRafat0@users.noreply.github.com>
2026-04-12 08:56:19 +0200

    Time: 7 ms (70.68%) | Memory: 9.5 MB (75.90%) - LeetSync

---
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int static const N = 305;
+    int dp[N][26][26];
+    pair<int, int> pos[26];
+    string s;
+    int dis(pair<int, int> a, pair<int, int> b) {
+        return abs(a.first - b.first) + abs(a.second - b.second);
+    }
+    int rec(int i, int a, int b) {
+        if (i == s.size()) return 0;
+        int &ret = dp[i][a][b];
+        if (~ret) return ret;
+        int L = rec(i + 1, s[i] - 'A', b) + dis(pos[s[i] - 'A'], pos[a]);
+        int R = rec(i + 1, a, s[i] - 'A') + dis(pos[s[i] - 'A'], pos[b]);
+        return ret = min(L, R);
+    }
+    int minimumDistance(string word) {
+        for (int i = 0; i < 26; ++i) {
+            pos[i] = {i / 6, i % 6};
+        }
+        this->s = word;
+        memset(dp, -1, sizeof dp);
+        int ans = 1e9;
+        for (int a = 0; a < 26; ++a) {
+            for (int b = 0; b < 26; ++b) {
+                ans = min(ans, rec(0, a, b));
+            }
+        }
+        return ans;
+    }
+};
