40982a4773b56332b67b689582f0fa74567347b8
Fady Rafat <121770425+FadyRafat0@users.noreply.github.com>
2026-07-16 09:55:05 +0300

    Time: 0 ms (100.00%) | Memory: 8.5 MB (46.98%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int a = 0, b = 0;
+        for (int i = 1; i <= n; ++i) {
+            a += i * 2 - 1;
+            b += i * 2;
+        } 
+        return gcd(a, b);
+    }
+};
